e1a897f8c2202b481f31cfbb3d010096f312beca
YunjieZhang200208 <y3446zha@gmail.com>
2023-03-08 21:48:17 -0500

    Updates

---
+++ precache-manifest.d3c74e868dee8fe0b219b7bd83822697.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10e1b3c42917d6e3065f1fbe69391480",
+    "url": "/msAbilities/index.html"
+  },
+  {
+    "revision": "b4b4a1d0a7c0705e18a3",
+    "url": "/msAbilities/static/css/main.cd47f4cb.chunk.css"
+  },
+  {
+    "revision": "207fb2712c8f6e32ead4",
+    "url": "/msAbilities/static/js/2.2b861a60.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/msAbilities/static/js/2.2b861a60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4b4a1d0a7c0705e18a3",
+    "url": "/msAbilities/static/js/main.b33ddb06.chunk.js"
+  },
+  {
+    "revision": "15160c4af50cb00028fc",
+    "url": "/msAbilities/static/js/runtime-main.1e1c8f2e.js"
+  },
+  {
+    "revision": "5d7f6abcc7757f45d1d16a0df5f7a8b0",
+    "url": "/msAbilities/static/media/homePic.5d7f6abc.png"
+  },
+  {
+    "revision": "97da397785ba592348ace3af2475b975",
+    "url": "/msAbilities/static/media/learning.97da3977.jpg"
+  },
+  {
+    "revision": "c6856f3cb3379a8d1b60d02a326a3ddb",
+    "url": "/msAbilities/static/media/logo.c6856f3c.png"
+  },
+  {
+    "revision": "1268a4e185cd1cacf799744bf3b632eb",
+    "url": "/msAbilities/static/media/teamPic.1268a4e1.jpg"
+  }
+]);
